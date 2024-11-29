08f271d6437a9143f5c6b40ea24d141975c86db7
Donghan Bian <59368264+Kepler1908@users.noreply.github.com>
2024-11-29 10:01:04 +0100

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -604,8 +604,8 @@ if llm_plots:
 # -----------------------
 
 st.header("Disagreement Analysis")
-st.write(update_comprehensive_results())
 update_comprehensive_results = update_comprehensive_results()
+st.write(update_comprehensive_results)
 filtered_data = calculate_disagreement_degree(update_comprehensive_results)
 display_disagreement_analysis(filtered_data)
 
